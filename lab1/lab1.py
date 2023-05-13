2a34ead74d1c0ba8352fa1d3d8a001316f3f4d50
ladaoot <ladachumakova@yandex.ru>
2023-05-13 23:00:30 +0300

    The quadratic equation

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -4,14 +4,14 @@ a = float(input("a = "))
 b = float(input("b = "))
 c = float(input("c = "))
 
-disct = b*b -4*a*c
+disct = b * b - 4 * a * c
 
-if disct>0 :
-    x1=(-b+math.sqrt(disct))/(2*a)
-    x2 =(-b-math.sqrt(disct))/(2*a)
-    print("x1=%.2f \nx2=%.2f" %(x1, x2))
-elif disct==0:
-    x = -b/(2*a)
+if disct > 0:
+    x1 = (-b + math.sqrt(disct)) / (2 * a)
+    x2 = (-b - math.sqrt(disct)) / (2 * a)
+    print("x1=%.2f \nx2=%.2f" % (x1, x2))
+elif disct == 0:
+    x = -b / (2 * a)
     print("x = %.2f" % x)
-else :
+else:
     print("Корней нет")
